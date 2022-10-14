2759477b1909a9b38ddf8d4c71598840eb8016a7
Kevin Michael Schott <kevin@kms695.de>
2022-10-14 16:04:08 +0200

    Absent message if user "leaves" tab

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+	import { browser } from '$app/environment'
 	import 'virtual:windi-components.css'
 	import 'virtual:windi-utilities.css'
 	import '@/css/app.scss'
+
+	let tabActive = true
+	if (browser) {
+		document.addEventListener('visibilitychange', () => (tabActive = !document.hidden))
+	}
 </script>
+
+<svelte:head>
+	{#if tabActive}
+		<title>Kevin Michael Schott 🧑‍💻</title>
+	{:else}
+		<title>(1) Why are you running?</title>
+	{/if}
+</svelte:head>
 
 <main class="flex flex-col items-center p-lg m-auto text-left w-full">
 	<slot />
